174ffb97163e9fc5ff2f3b1384aac0d0244add8f
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-08-05 20:30:21 -0700

    add apollo server package and added it to express as a middleware

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const path = require('path');
+//add Apollo server package
+//const { ApolloServer } = require('apollo-server-express');
 const db = require('./config/connection');
 const routes = require('./routes');
+//add typeDefs and resolvers from schema folder
+//const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//Set up server instance
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+// });
+
+//apply server to our middleware
+// server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
