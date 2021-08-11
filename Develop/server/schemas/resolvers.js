e0e219bdb0c7231873b548f5cec4fdb01dd62d65
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-08-10 19:27:47 -0700

    updated resolvers.js to use AuthenticationError tool and add comments

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -1,22 +1,25 @@
 //import models
+const { AuthenticationError } = require('apollo-server-express');
 const { User } = require('../models');
-const { authMiddleware, signToken } = require('../utils/auth');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
+    //finds user using context
     me: async (parents, args, context) => {
       if (context.user) {
-        const user = await User.find({});
+        const user = await User.findOne({ _id: context.user._id });
 
         if (user) {
           return user;
         }
       }
 
-      console.log('please login');
+      throw new AuthenticationError('You need to be logged in!');
     },
   },
   Mutation: {
+    //creates a new user with valid login token
     addUser: async (parents, args) => {
       const user = await User.create(args);
       console.log(user);
@@ -24,10 +27,10 @@ const resolvers = {
 
       return { token, user };
     },
-    //
-    login: async (parents, { email, password }) => {
-      const user = await User.find({ email });
 
+    //checks if user has entered correct email and password for log in
+    login: async (parents, { email, password }) => {
+      const user = await User.findOne({ email });
       if (user) {
         const correctPw = await User.isCorrectPassword(password);
 
@@ -40,19 +43,21 @@ const resolvers = {
             console.error('failed to create token');
           }
         } else {
-          console.error('wrong password :(');
+          throw new AuthenticationError('Invalid profile email or password!');
         }
       } else {
         console.error('failed to find user. check seeds or create this user');
         return;
       }
     },
+
+    // logged in users can add a book to their saved library
     saveBook: async (parents, { bookId, book }, context) => {
+      //if user is logged in and has valid jwt then allow functionality
       if (context.user) {
         return Profile.findOneAndUpdate(
           { _id: bookId },
           {
-            //do I  add all book fields in the object??
             $addToSet: { savedBooks: book },
           },
           {
@@ -60,12 +65,21 @@ const resolvers = {
             runValidators: true,
           }
         );
-      } else {
-        console.error('failed to find user. check seeds or create this user');
-        return;
-      }
+      } //throw error if user isn't logged in
+      throw new AuthenticationError('You need to be logged in!');
     },
-    // removeBook: async (parents, args) => {},
+
+    // logged in users can remove a book from their saved library
+    removeBook: async (parents, { book }, context) => {
+      if (context.user) {
+        return Profile.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: book } },
+          { new: true }
+        );
+      } //throw error if user isn't logged in
+      throw new AuthenticationError('You need to be logged in!');
+    },
   },
 };
 
